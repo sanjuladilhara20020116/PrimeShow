24dac03d7e184b8efe4d606a4112df56c9fc1223
Sanjula Dilhara Haputhanthrige <dilhara2002sanju@gmail.com>
2025-12-26 19:14:55 +0530

    Update index.js

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -13,7 +13,7 @@ const syncUserCreation = inngest.createFunction(
     const{id,first_name,last_name,email_addresses,image_url}=event.data
     const userData={
       _id:id,
-      email:email_addresses[0].email_addresse,
+      email:email_addresses[0].email_address,
       name:first_name+ '' + last_name,
       image:image_url
     }
